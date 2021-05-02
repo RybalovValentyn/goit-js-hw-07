3bbbf0cac6670f19171fcf34c3c8f922c16c0c50
RybalovValentyn <0661629069@ukr.net>
2021-05-02 17:11:10 +0300

    2.1

--- js/task-02.js
+++ js/task-02.js
@@ -6,15 +6,25 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
-  const products = [];
+//   const products = [];
 const productList = document.querySelector('#ingredients')
-console.log(productList);
-for (const ingredient of ingredients) {
-    const titleEl = document.createElement('li')
-    titleEl.textContent = ingredient;
+// console.log(productList);
+// for (const ingredient of ingredients) {
+//     const titleEl = document.createElement('li')
+//     titleEl.textContent = ingredient;
 
-    console.log(titleEl);
-      products.push(titleEl)
-  };
+//     console.log(titleEl);
+//       products.push(titleEl)
+//   };
+
+const products = ingredients.map(product =>{
+        const titleEl = document.createElement('li')
+    titleEl.textContent = product;
+    return titleEl;
+})
+
+
   console.log(products);
   productList.append(...products)
+
+
